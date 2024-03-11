8a66bb8cf249fa6f61658df443cd7f378f91bf11
blakekimballdev <blake.kimball.dev@gmail.com>
2024-03-11 09:10:23 -0400

    DX

--- src/Convert.js
+++ src/Convert.js
@@ -1,7 +1,7 @@
-const offsetToRowCol = (offset, gridSize=8) => {
-    const row = Math.floor(offset / gridSize);
-    const col = offset % gridSize;
-    return [row, col];
-};
+const offsetToRowCol = (offset, gridSize = 8) => {
+  const row = Math.floor(offset / gridSize)
+  const col = offset % gridSize
+  return [row, col]
+}
 
-export default offsetToRowCol;
+export default offsetToRowCol
